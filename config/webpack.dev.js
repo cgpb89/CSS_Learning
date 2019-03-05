1994aed30affc3cdd2a84caeefbb11563316f14b
Pereira <cpereira@dxc.com>
2019-03-05 11:20:20 -0600

    First Commit. Main Nav Bar

---
+++ config/webpack.dev.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: ['./src/index.js']
+    },
+    mode: 'development',
+    output: {
+        filename: '[name]-bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: 'dist',
+        overlay: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: [
+                    { loader: 'babel-loader' }
+                ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }]
+            }, {
+                test:/\.(s*)css$/,
+                use: ['style-loader','css-loader','sass-loader' ]
+            }, {
+                test: /\.html$/,
+                use: [
+                    { loader: 'html-loader' }
+                ]
+            }
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ]
+}
