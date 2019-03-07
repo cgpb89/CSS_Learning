9946d065cc39c5f706a1fa7b53244a87e09424e9
Pereira <cpereira@dxc.com>
2019-03-07 13:48:16 -0600

    NavBar responsive. Main images done.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -39,6 +39,9 @@ module.exports = {
                 use: [
                     { loader: 'html-loader' }
                 ]
+            },{
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: "file-loader"
             }
 
         ]
